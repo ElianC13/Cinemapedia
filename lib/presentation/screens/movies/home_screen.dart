598ca5663fafa3eb278d86b729fa80cc582c5c01
unknown <elian.coutsiers20@gmail.com>
2024-02-21 12:00:25 -0300

    Swiper, slidesShow

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/presentation/provider/movies/movies_providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -41,14 +42,13 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
     if(nowPlayingMovies.length == 0) return CircularProgressIndicator();
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(
-          title: Text(movie.title),
-        );
-      },
+    return Column(
+      children: [
+
+        CustomAppbar(),
+
+        MoviesSlideShow(movies: nowPlayingMovies)
+      ],
     );
   }
 }
