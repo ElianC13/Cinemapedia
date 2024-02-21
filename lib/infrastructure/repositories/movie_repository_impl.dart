d33cac14c7175907d069380228003c0386a601dc
unknown <elian.coutsiers20@gmail.com>
2024-02-21 00:00:43 -0300

    NowPlaying provider

---
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,15 @@
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/repositories/movies_repository.dart';
+
+class MovieRepositorieImpl extends MoviesRepositorie {
+
+  final MoviesDatasource datasource;
+  MovieRepositorieImpl(this.datasource);
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return datasource.getNowPlaying(page: page);
+  }
+
+}
