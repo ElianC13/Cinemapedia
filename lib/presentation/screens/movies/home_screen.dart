0587fd450416a21dc469fbf300ddc52320a9f90e
unknown <elian.coutsiers20@gmail.com>
2024-02-20 21:16:21 -0300

    moviedb response

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,6 +1,4 @@
-import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,9 +7,9 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
-        child: Text(Enviroment.theMovieDbKey),
+        child: Placeholder(),
         
       ),
     );
