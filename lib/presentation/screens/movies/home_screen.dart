07196ee8883464e37f48b667767b85280d5c5607
unknown <elian.coutsiers20@gmail.com>
2024-02-21 23:31:11 -0300

    FullScreenLoader

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -50,6 +50,8 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
     if(slideShowMovies.isEmpty) return const CircularProgressIndicator();
 
+    return FullScreenLoader();
+
     return CustomScrollView(
       slivers: [
 
