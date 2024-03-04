2c8c1490494ad9ff8f42999f7f27ac3ef81e807e
unknown <elian.coutsiers20@gmail.com>
2024-03-04 16:03:07 -0300

    final seccion 16

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,113 +1,33 @@
-import 'package:cinemapedia/presentation/provider/providers.dart';
+import 'package:cinemapedia/presentation/screens/views/views.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
   static const name = 'home-screen';
+  final int pageIndex;
+
+  const HomeScreen({
+    super.key, 
+    required this.pageIndex
+  });
+
+  final viewRoutes = const <Widget>[
+    HomeView(),
+    SizedBox(),
+    FavoritesView(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: _HomeView(),
-      ),
-      bottomNavigationBar: CustomBottonNavigation(),
-    );
-  }
-}
-
-class _HomeView extends ConsumerStatefulWidget {
-  const _HomeView();
-
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-
-  @override
-  void initState() {
-    super.initState();
-
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-    ref.read(popularMoviesProvider.notifier).loadNextPage();
-    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    final initialLoading = ref.watch(initialLoadingProvider);
-    if( initialLoading ) return const FullScreenLoader();
-
-    final slideShowMovies = ref.watch(moviesSlideshowProvider);
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    final popularMovies = ref.watch(popularMoviesProvider);
-    final upcomingMovies = ref.watch(upcomingMoviesProvider);
-    final topRatedMovies = ref.watch(topRatedMoviesProvider);
-
-
-    if(slideShowMovies.isEmpty) return const CircularProgressIndicator();
-
-    return CustomScrollView(
-      slivers: [
-
-        const SliverAppBar(
-          floating: true,
-          title: CustomAppbar(),
-          flexibleSpace: FlexibleSpaceBar(
-          ),
+        child: IndexedStack(
+          index: pageIndex,
+          children: viewRoutes,
         ),
-
-        SliverList(delegate: SliverChildBuilderDelegate(
-          (context, index){
-           return Column(
-              children: [
-
-                MoviesSlideShow(movies: slideShowMovies),
-            
-                MovieHorizontalListview(
-                  movies: nowPlayingMovies,
-                  title: 'En Cines',
-                  subTitle: 'Lunes 20',
-                  loadNextPage: () => ref.read(nowPlayingMoviesProvider.notifier).loadNextPage(),
-                ),
-            
-                MovieHorizontalListview(
-                  movies: upcomingMovies,
-                  title: 'Proximamente',
-                  subTitle: 'Este mes',
-                  loadNextPage: () => ref.read(upcomingMoviesProvider.notifier).loadNextPage(),
-                ),
-            
-                MovieHorizontalListview(
-                  movies: popularMovies,
-                  title: 'Populares',
-                  //subTitle: 'Este mes',
-                  loadNextPage: () => ref.read(popularMoviesProvider.notifier).loadNextPage(),
-                ),
-            
-                MovieHorizontalListview(
-                  movies: topRatedMovies,
-                  title: 'Mejor Puntuadas',
-                  subTitle: 'Desde siempre',
-                  loadNextPage: () => ref.read(topRatedMoviesProvider.notifier).loadNextPage(),
-                ),
-
-                const SizedBox(height: 10,)
-              ],
-            );
-          },
-          childCount: 1 
-        ))
-
-      ]
-      
-      
+      ),
+      bottomNavigationBar: CustomBottonNavigation( currentIndex : pageIndex),
     );
   }
 }
+
